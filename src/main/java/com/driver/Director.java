ff9423e8c2d552652ead49d0f2b6b39003afd661
Jayantjain1 <jjayant511@gmail.com>
2022-12-31 16:30:18 +0530

    watch

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -1,33 +1,44 @@
 package com.driver;
 
 public class Director {
-    public String name;
-    public int numberOfMovies;
-    public double imdbRating;
-    public Director(){
+
+    private String name;
+
+    private int numberOfMovies;
+
+    private double imdbRating;
+
+    public Director() {
 
     }
-    public Director(String name,int numberOfMovies,double imdbRating){
+
+    public Director(String name, int numberOfMovies, double imdbRating) {
         this.name = name;
         this.numberOfMovies = numberOfMovies;
         this.imdbRating = imdbRating;
     }
-    public void setName(String name){
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
-    public void setImdbRating(double imdbRating){
-        this.imdbRating = imdbRating;
+
+    public int getNumberOfMovies() {
+        return numberOfMovies;
     }
-    public void setNumberOfMovies(int numberOfMovies){
+
+    public void setNumberOfMovies(int numberOfMovies) {
         this.numberOfMovies = numberOfMovies;
     }
-    public String getName(){
-        return this.name;
+
+    public double getImdbRating() {
+        return imdbRating;
     }
-    public int getNumberOfMovies(){
-        return this.numberOfMovies;
-    }
-    public double getImdbRating(){
-        return this.imdbRating;
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
     }
 }
--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,35 +1,41 @@
 package com.driver;
 
 public class Movie {
-    public String name;
-    public int durationInMinutes;
-    public double imdbRating;
-    public Movie() {
+
+    private String name;
+    private int durationInMinutes;
+    private double imdbRating;
+
+    public Movie(){
 
     }
-    public Movie(String name,int durationInMinutes,double imdbRating) {
+    public Movie(String name, int durationInMinutes, double imdbRating) {
         this.name = name;
         this.durationInMinutes = durationInMinutes;
         this.imdbRating = imdbRating;
     }
-    public void setName(String name){
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
-    public void setDurationInMinutes(int durationInMinutes){
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
         this.durationInMinutes = durationInMinutes;
     }
-    public void setImdbRating(double imdbRating){
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
         this.imdbRating = imdbRating;
     }
-
-    public String getName(){
-        return this.name;
-    }
-    public int getDurationInMinutes(){
-        return this.durationInMinutes;
-    }
-    public double getImdbRating(){
-        return this.imdbRating;
-    }
-
 }
